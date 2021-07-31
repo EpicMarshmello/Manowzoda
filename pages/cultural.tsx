3be1319e6f81bc7bf1d3ee58734fd96d6d2ae640
BrownPharadol <brown01945@gmail.com>
2021-07-31 21:54:39 +0700

    add map

--- pages/cultural.tsx
+++ pages/cultural.tsx
@@ -2,13 +2,13 @@ import React from "react";
 
 export default function Cultural() {
   return (
-    <div className="h-full w-full bg-gray-200 ">
-      <div className="h-5/6 w-4/5 ml-56 pt-20 flex justify-center">
+    <div className="h-full w-full bg-gray-200 pt-20">
+      {/* <div className="h-5/6 w-4/5 ml-56 pt-20 flex justify-center"> */}
         <div className="bg-green-300 border-8 border-white w-full rounded-2xl text-white font-bold text-4xl flex justify-center items-center">
           {" "}
-          อันนี้เป็นแผนที่อะ แต่ขกใส่ให้
+          <img src="../map.jpg" alt="" className="w-7/12"/>
         </div>
-      </div>
+      {/* </div> */}
     </div>
   );
 }
